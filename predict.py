f89d56d27d60e2539e5400f89c280580adefb5c5
Joseph-Ajegetina <josephajongbah@gmail.com>
2022-12-05 10:40:12 +0000

    Added necessary files

---
+++ predict.py
@@ -0,0 +1,160 @@
+
+import argparse
+import torch
+import json
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+import os
+import random
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+## A function that loads a checkpoint and rebuilds the model
+
+def load_checkpoint(filepath):
+	model = models.resnet18(pretrained=True)
+
+	
+	checkpoint = torch.load(filepath)
+	lr = checkpoint['learning_rate']
+	model.fc = checkpoint['classifier']
+	model.load_state_dict(checkpoint['model_state_dict'])
+	model.class_to_idx = checkpoint['class_to_idx']
+	optimizer = optim.Adam(model.fc.parameters(), lr=lr)
+	optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+	input_size = checkpoint['input_size']
+	output_size = checkpoint['output_size']
+	epoch = checkpoint['epoch']
+
+
+	return model, optimizer, input_size, output_size, epoch
+
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    # loading the image from the path
+    loaded_image = Image.open(image)
+    transform_image = transforms.Compose([transforms.Resize(255),
+                                     transforms.CenterCrop(224),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406],
+                                                          [0.229, 0.224, 0.225])])
+    transformed_image_tensor = transform_image(loaded_image)
+    return transformed_image_tensor
+
+
+def predict(image_path, model, topk=5):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+    
+    # TODO: Implement the code to predict the class from an image file
+    image_processed = process_image(image_path).to(device)
+
+    # Getting the image for 1 format
+    image_processed.unsqueeze_(0)
+ 
+    
+    log_ps = model.forward(image_processed)
+    ps = torch.exp(log_ps)
+    top_ps,top_class = ps.topk(5)
+
+    # Since we have labels and probs of prediction we need a table to look up the idx from the class
+    idx_to_class = {}
+    
+    # creating a class -> idx lookup 
+    for key, value in model.class_to_idx.items():
+        idx_to_class[value] = key
+
+    top_labels_numpy = top_class[0].cpu().numpy()
+
+    top_labels = []
+    for label in top_labels_numpy:
+        top_labels.append(int(idx_to_class[label]))
+
+    # since we have the idx of the top labels we can look up name from cat_to_name
+    top_flowers = [cat_to_name[str(label)] for label in top_labels]
+    
+    return top_ps, top_labels, top_flowers
+
+# Initialization using question configurations
+parser = argparse.ArgumentParser(description="This program predicts flowers' names from their images",
+								 usage='''
+        needs a saved checkpoint
+        python predict.py ( use default image 'flowers/test/1/image_06743.jpg' and root directory for checkpoint)
+        python predict.py /path/to/image checkpoint (predict the image in /path/to/image using checkpoint)
+        python predict.py --top_k 3 (return top K most likely classes)
+        python predict.py --category_names cat_to_name.json (use a mapping of categories to real names)
+        python predict.py --gpu (use GPU for inference)''',
+								 prog='predict')
+
+## Get path of image
+parser.add_argument('path_to_image', action="store", nargs='?', default='flowers/test/99/image_07833.jpg', help="path/to/image")
+
+parser.add_argument('path_to_checkpoint', action="store", nargs='?', default='checkpoint.pth', help="path/to/checkpoint")
+## set top_k
+parser.add_argument('--top_k', action="store", default=1, type=int, help="enter number of guesses", dest="top_k")
+## Choose json file:
+parser.add_argument('--category_names', action="store", default="cat_to_name.json", help="get json file", dest="category_names")
+## Set GPU
+parser.add_argument('--gpu', action="store_true", default=False, help="Select GPU", dest="gpu")
+
+## Get the arguments
+args = parser.parse_args()
+
+path_to_image =  args.path_to_image
+path_to_checkpoint = args.path_to_checkpoint
+top_k =  args.top_k
+category_names =  args.category_names
+# Use GPU if it's selected by user and it is available
+if args.gpu and torch.cuda.is_available(): 
+	gpu = args.gpu
+# if GPU is selected but not available use CPU and warn user
+elif args.gpu:
+	gpu = False
+	print('GPU is not available, will use CPU...')
+
+else:
+	gpu = args.gpu
+
+# Use GPU if it's selected by user and it is available
+device = torch.device("cuda" if gpu else "cpu")
+print('Will use {} for prediction...'.format(device))
+
+print("\nPath of image: {} \nPath of checkpoint: {} \nTopk: {} \nCategory names: {} ".format(path_to_image, path_to_checkpoint, top_k, category_names))
+print('GPU: ', gpu)
+print()
+
+## Label mapping
+with open(category_names, 'r') as f:
+    cat_to_name = json.load(f)
+
+## Loading model
+print('Loading model........................ ')
+print()
+
+model, my_optimizer, input_size, output_size, epoch  = load_checkpoint(path_to_checkpoint)
+
+if(gpu):
+    model.cuda()
+else:
+    model.cpu()
+
+        
+model.eval()
+
+# Make prediction
+top_probs, top_labels, top_flowers = predict(path_to_image, model) 
+top_probs = top_probs[0].detach().cpu().numpy()
+
+flower_num = path_to_image.split('/')[2]
+title_ = cat_to_name[flower_num]
+
+print(f'actual label {title_}\n')
+for counter in range(top_k):
+     print(f'{top_flowers[counter]}\t{top_probs[counter]:.3f} %')
---
+++ train.py
@@ -0,0 +1,313 @@
+
+import argparse
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+import os
+from collections import OrderedDict
+
+
+
+# Initialization
+parser = argparse.ArgumentParser(description='Model for training flowers dataset using pytorch',
+								 usage='''
+        python train.py (data set shall be initially extracted to the 'flowers' directory)
+        python train.py data_dir (data set shall be initially extracted to the 'data_dir' directory)
+        python train.py data_dir --save_dir save_directory (set directory to save checkpoints)
+        python train.py data_dir --arch "vgg13" (choose between vgg13 and resnet18)
+        python train.py data_dir --learning_rate 0.01 --hidden_units [1024, 512, 256] --epochs 20 (set hyperparameters)
+        ''',
+prog='train')
+
+
+## Adding argument for data directory with default of flowers and help messages
+parser.add_argument('data_directory', action="store", nargs='?', default="flowers", help="The dataset directory for the model")
+
+## Adding argument for save directory 
+parser.add_argument('--save_dir', action="store", default="", dest="save_directory", help="Checkpoint directory for train model state")
+
+## Adding architecture argument with choices
+parser.add_argument('--arch', action="store", default="resnet18", choices=['vgg13', 'resnet18'],
+					 help="you can only choose vgg13 or resnet", dest="architecture")
+
+## Argument for setting hyperparameters
+parser.add_argument('--learning_rate', action="store", default="0.003", type=float, help="This sets the learning rate",
+					 dest="learning_rate")
+parser.add_argument('--hidden_units', action="store", nargs=3, default=[1024, 512], type=int, help="Enter the hidden units parameters",
+					 dest="hidden_units")
+parser.add_argument('--epochs', action="store", default=3, type=int, help="This sets the epochs of training", dest="epochs")
+
+## Set GPU
+parser.add_argument('--gpu', action="store_true", default=False, help="Use GPU", dest="gpu")
+
+## Parsing arguments to get their value
+args = parser.parse_args()
+
+data_dir =  args.data_directory
+save_dir =  args.save_directory
+arch=  args.architecture
+lr = args.learning_rate
+hidden_units = args.hidden_units
+epochs = args.epochs
+                                                                        
+# Condition for selecting GPU if it's available
+                                                                        
+if args.gpu and torch.cuda.is_available(): 
+	gpu = args.gpu
+                                                                        
+# Warn user of inability to gpu if unavailable
+elif args.gpu:
+	gpu = False
+	print('CPU will be used as GPU is not available on this machine.')
+
+else:
+	gpu = args.gpu
+
+# Printing the configuration details from args
+print(f'''Data directory: {data_dir} \nSave directory: {save_dir} \nArchitecture: {arch}.
+\nLearning rate: {lr} 
+ \nHidden Units: {hidden_units}
+ \nEpochs: {epochs} 
+ \nUsing GPU: {gpu}''')
+
+
+
+## set data directory locations
+
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
+
+## Define transforms for the training, validation, and testing sets
+                                                                        
+data_transforms = {'train':transforms.Compose([transforms.RandomRotation(30),
+                                     transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406],
+                                                          [0.229, 0.224, 0.225])]),
+                   'valid':transforms.Compose([transforms.Resize(255),
+                                     transforms.CenterCrop(224),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406],
+                                                          [0.229, 0.224, 0.225])]),
+                                    
+                   'test':transforms.Compose([transforms.Resize(255),
+                                     transforms.CenterCrop(224),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406],
+                                                          [0.229, 0.224, 0.225])]),
+                  }
+
+dataset_categories = ['train', 'valid', 'test']
+                                                                        
+image_datasets = {category:datasets.ImageFolder(os.path.join(data_dir, category), transform=data_transforms[category]) 
+                  for category in dataset_categories}
+
+dataloaders = {category:torch.utils.data.DataLoader(image_datasets[category], batch_size=64, shuffle=True)
+                  for category in dataset_categories}
+## Build and train the network
+
+# download architecture
+if arch == 'vgg13':
+	print('Downloading VGG-13 pretrained model ...')
+	model = models.vgg13(pretrained=True)                                                                     
+
+if arch == 'resnet18':
+	print('Downloading RESNET18 ...')
+	model = models.resnet18(pretrained=True)
+
+
+# detting device based on user args received
+device = torch.device("cuda" if gpu else "cpu")
+
+
+# Freeze parameters so we don't backprop through them
+for param in model.parameters():
+    param.requires_grad = False
+
+if arch.upper()  == 'VGG13':                                                                     
+    model.classifier = nn.Sequential(nn.Linear(25088, hidden_units[0]),
+                                     nn.ReLU(),
+                                     nn.Dropout(0.2),
+                                     nn.Linear(hidden_units[0], hidden_units[1]),
+                                     nn.ReLU(),                               
+                                     nn.Linear(hidden_units[1], 102),
+                                     nn.LogSoftmax(dim=1))
+                                                                       
+    # Only train the classifier parameters, feature parameters are frozen
+    optimizer = optim.Adam(model.classifier.parameters(), lr=lr)
+                                                                        
+                                                                        
+if arch.upper() == 'RESNET18':
+    classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(512, 256)),
+                          ('relu1', nn.ReLU()),
+                          ('dpout1', nn.Dropout(0.2)),
+                          ('fc2', nn.Linear(256, 102)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+    
+    model.fc = classifier
+    optimizer = optim.Adam(model.fc.parameters(), lr=lr)
+
+
+model.to(device)
+if torch.cuda.is_available() and device == 'gpu':
+   model.cuda()
+
+                                                                        
+
+criterion = nn.NLLLoss()
+
+                                                                        
+print('\n\n\nTraining the model................... ')
+print('Do not turn off your computer........ ')
+print('..................................... ')
+
+steps = 0
+running_loss = 0
+print_every = 10
+for epoch in range(epochs):
+    trainloader = dataloaders['train']
+    for inputs, labels in trainloader:
+        steps += 1
+        # Move input and label tensors to the default device
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        
+        if steps % print_every == 0:
+            valid_loss = 0
+            accuracy = 0
+            model.eval()
+            with torch.no_grad():
+                validloader = dataloaders['valid']
+                for inputs, labels in validloader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    logps = model.forward(inputs)
+                    batch_loss = criterion(logps, labels)
+                    
+                    valid_loss += batch_loss.item()
+                    
+                    # Calculate accuracy
+                    ps = torch.exp(logps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                    
+            print(f"Epoch {epoch+1}/{epochs}.. "
+                  f"Train loss: {running_loss/print_every:.3f}.. "
+                  f"Validation loss: {valid_loss/len(validloader):.3f}.. "
+                  f"Validation accuracy: {accuracy/len(validloader):.3f}")
+            running_loss = 0
+            model.train()
+print()
+print('\nValidating the model......................................................... ')
+print('Do not turn off your computer..................................................\n................................................................................. ')
+
+## Do validation on the test set
+
+# number of total true classifications
+total = 0
+# number of total images tested
+total_length = 0
+# total accuracy for test dataset (calculated so far)
+total_accuracy = 0
+# batch number
+batch = 0
+testloader = dataloaders['test']
+for inputs, labels in testloader:
+    batch += 1    
+    # Move input and label tensors to the default device
+    inputs, labels = inputs.to(device), labels.to(device)
+   
+    
+    accuracy = 0
+    model.eval()
+    with torch.no_grad():
+
+        logps = model.forward(inputs)
+                 
+        # Calculate accuracy
+        ps = torch.exp(logps)
+        top_p, top_class = ps.topk(1, dim=1)
+        matches = top_class == labels.view(*top_class.shape)
+        
+        # this is batch accuracy
+        accuracy += torch.mean(matches.type(torch.FloatTensor)).item()
+                                                                        
+        # number of true classifications so far
+        total += torch.sum(equals)
+        # number of classification attempts so far
+        total_length += len(equals)
+        
+        total_accuracy = total.item()/total_length
+    print(f"Batch {batch}.. "
+          f"Accuracy: {accuracy*100:.3f}%.. "
+          f"Total Accuracy: {total_accuracy*100:.3f}%")
+
+    model.train()
+
+                                                                        
+## Save the checkpoint 
+print('\nSaving the model..................... ')
+
+if save_dir:
+	if not os.path.exists(save_dir):
+		os.mkdir(arg_save_dir)
+		print("Directory " , save_dir ,  " has been created for saving checkpoints")
+	else:
+		print("Directory " , save_dir ,  " allready exists for saving checkpoints")
+	save_dir = save_dir + '/checkpoint.pth'
+else:
+	save_dir = 'checkpoint.pth'
+
+print()
+
+model.class_to_idx = image_datasets['train'].class_to_idx
+                                                                        
+checkpoint = {'input_size': 25088,
+              'output_size': 102,
+              'epoch': epochs,
+              'classifier': model.classifier if arch=='vgg13' else model.fc,
+              'optimizer_state_dict': optimizer.state_dict(),
+              'class_to_idx': model.class_to_idx,
+              'learning_rate': lr,
+              'model_state_dict': model.state_dict()}
+
+torch.save(checkpoint, save_dir)
+
+print('Model saved')
+print()
+
+## this part is for validating saved checkpoint
+
+def load_checkpoint(filepath):
+
+    checkpoint = torch.load(filepath)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    
+    input_size = checkpoint['input_size']
+    output_size = checkpoint['output_size']
+    epoch = checkpoint['epoch']
+    
+    return model, optimizer, input_size, output_size, epoch 
+
+print('\n\nValidating checkpoint and loading................ ')
+
+my_model, my_optimizer, input_size, output_size, epoch  = load_checkpoint(save_dir)
+
+
+print('Loaded saved model')
+print(my_model)
